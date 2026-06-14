cb5374164de581e173a2cea21759772fd0d68ac1
Sami20178 <docc.samisoliman@hotmail.com>
2026-06-14 10:55:58 +0200

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE accounts;
+USE accounts;
+
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+
+    birthday DATE NOT NULL,
+    gender VARCHAR(20) NOT NULL,
+
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL,
+
+    phone VARCHAR(30) UNIQUE NULL,
+
+    password_hash VARCHAR(255) NOT NULL,
+
+    email_code VARCHAR(10),
+    verified BOOLEAN DEFAULT FALSE,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
