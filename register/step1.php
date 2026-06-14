faa6695f67111af6c13205acc960c8403f706856
Sami20178 <docc.samisoliman@hotmail.com>
2026-06-14 10:51:52 +0200

    Update step1.php

--- register/step1.php
+++ register/step1.php
@@ -1,1 +1,39 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta charset="UTF-8">
+<title>Konto erstellen</title>
+<link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
 
+<div class="card">
+
+<h1>Konto erstellen</h1>
+
+<p>Schritt 1 von 5</p>
+
+<form action="step2.php" method="POST">
+
+<input
+class="input"
+name="firstname"
+placeholder="Vorname"
+required>
+
+<input
+class="input"
+name="lastname"
+placeholder="Nachname"
+required>
+
+<button class="btn">
+Weiter
+</button>
+
+</form>
+
+</div>
+
+</body>
+</html
